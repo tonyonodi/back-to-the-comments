571011a8f1fc86e414ec13ecd276a35ee1b23037
Tony Onodi <tony@Tonys-MacBook-Pro.local>
2014-06-14 21:34:57 +0100

    Added comments.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+// Function to check if location is hacker news.
 function checkLocation() {
 	var hackerNewsHost;
 
 	hackerNewsHost = "news.ycombinator.com";
 
+	// return true or false.
 	if ( location.host == hackerNewsHost ) {
 		return true;
 	} else {
@@ -10,10 +13,12 @@ function checkLocation() {
 	}
 }
 
+// This is where the magic happens,
 function main() {
 	console.log("running main");
 }
 
+// check if the page is hacker news and run main if so.
 var locationIsHackerNews;
 
 locationIsHackerNews = checkLocation();
