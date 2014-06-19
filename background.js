5f1dc21015cef08f1b539f8cb9a521eb9d3bbfb7
Tony Onodi <tony@Tonys-MacBook-Pro.local>
2014-06-20 00:58:49 +0100

    Compares stored and current URL but finds them not to be equivalent.

--- background.js
+++ background.js
@@ -6,6 +6,21 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		// 	console.log("On hacker news.");
 		// 	chrome.tabs.executeScript(tabId, {file: "inject.js"});
 		// }
+
+		// get the object stored in local storage
+		chrome.storage.local.get(null, function(savedData) {
+			var storedURL = savedData.linkURL,
+				currentURL = window.location;
+
+			console.log(storedURL);
+			console.log(currentURL);
+
+			if (storedURL == currentURL) {
+				console.log("HN page!");
+			} else {
+				console.log("not a HN page");
+			}
+		});
 		
 		chrome.tabs.sendMessage(tabId, {data: tab}, function(response) {
 			console.log(response);
