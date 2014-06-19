e780d10c2ab8249a51dc15be4064e1e94087e29d
Tony Onodi <tony@Tonys-MacBook-Pro.local>
2014-06-19 21:35:08 +0100

    Background.js now listens for page change

--- background.js
+++ background.js
@@ -1,1 +1,6 @@
-console.log("loaded background.js");
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if ( changeInfo && changeInfo.status == "complete" ) {
+		console.log( "Tab updated: " + tab.url );
+	}
+});
