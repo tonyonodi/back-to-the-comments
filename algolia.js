5be60650a5b7416104a6c535b0a84d18fe0f22e9
Tony Onodi <tonyonodi@gmail.com>
2014-12-17 02:13:02 +0000

    Now has array of objects with relevant data.

--- algolia.js
+++ algolia.js
@@ -14,13 +14,33 @@ function nodelistToArray( nodelist ) {
 	return array;
 }
 
+var makePostObject = function( currentPost ) {
+	var postLinkNode,
+		commentURL,
+		postObject;
+
+	postLinkNode = currentPost.children[0].children[0];
+	commentURL = currentPost.children[4].children[0].getAttribute("href");
+
+	// construct post object
+	postObject = {
+		"link": postLinkNode,
+		"commentURL": commentURL
+	}
+
+	return postObject;
+}
+
 var main = function() {
 	var postList,
-		postArray;
+		postArray,
+		postObjectArray;
 
 	// get nodelist of post elements and convert to array
 	postList = document.querySelectorAll( "div.hit div.title_url" );
 	postArray = nodelistToArray( postList );
+
+	postObjectArray = postArray.map( makePostObject );
 }
 
 window.onload = main;
