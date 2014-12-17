065c9033c4efcd13e37955e15d5f6c6eaf761331
Tony Onodi <tonyonodi@gmail.com>
2014-12-17 01:54:47 +0000

    Gets a nodelist of all links.

--- algolia.js
+++ algolia.js
@@ -1,1 +1,7 @@
-console.log("Algolia is alive!");
+var main = function() {
+	console.log("hello");
+	linkList = document.querySelectorAll( "div.hit div.title_url div.title a" );
+	console.log(linkList);
+}
+
+window.onload = main;
