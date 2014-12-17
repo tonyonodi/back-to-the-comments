cae7ff3d54287d6384d92f7cc1b6881dcb51d179
Tony Onodi <tonyonodi@gmail.com>
2014-12-17 02:02:17 +0000

    Converts nodelist to array.

--- algolia.js
+++ algolia.js
@@ -1,7 +1,26 @@
+/**
+* Functions
+*/
+function nodelistToArray( nodelist ) {
+	var array = [];
+
+	for (var i = 0; i < nodelist.length; i++ ) {
+		var currentPost = nodelist[i];  // the node for the current post
+		
+		// Add node to array
+		array.push( currentPost );
+	}
+
+	return array;
+}
+
 var main = function() {
-	console.log("hello");
-	linkList = document.querySelectorAll( "div.hit div.title_url div.title a" );
-	console.log(linkList);
+	var postList,
+		postArray;
+
+	// get nodelist of post elements and convert to array
+	postList = document.querySelectorAll( "div.hit div.title_url" );
+	postArray = nodelistToArray( postList );
 }
 
 window.onload = main;
